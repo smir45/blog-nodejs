bd99d131f916721ebfd0fe6aababdccecf723a55
Samir Mishra <smir.mishra1551@gmail.com>
2021-07-14 22:49:26 +0545

    created a nodejs blog app

---
+++ routes/articles.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const Article = require("./../models/article")
+
+const router = express.Router();
+
+router.get("/new", (req, res) => {
+  res.render ('articles/new', {article: new Article()});
+})
+
+
+
+router.get('/:slug', async (req, res) => {
+  const article = await Article.findOne({slug: req.params.slug})
+  if(article == null) res.redirect('/')
+  res.render('articles/show', {article: article});
+})
+
+
+
+
+router.delete('/:id', async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id)
+  res.redirect('/')
+})
+
+
+
+router.post('/', async (req, res) => {
+    let article = new Article({
+      title: req.body.title,
+      description: req.body.description,
+      markdown: req.body.markdown
+    })
+
+   try{
+     article = await article.save()
+     res.redirect(`/articles/${article.slug}`)
+
+   } catch (e){
+     console.log(e)
+      res.render(`articles/new`, {article: article})
+   }
+})
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const article = require("./models/article");
+const Article = require("./models/article");
+const articleRouter = require("./routes/articles");
+
+mongoose.connect(
+  "mongodb+srv://smir45:bl0@tW@R33@cluster0.vzaxj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(methodOverride('_method'));
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ publishedAt: "desc" });
+
+  res.render("articles/index", { articles: articles });
+});
+app.use("/articles", articleRouter);
+app.listen(5000);
