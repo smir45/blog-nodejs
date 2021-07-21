1291a4101dc0f9c4b5cbc4944d0750cd653b9c73
Samir Mishra <smir.mishra1551@gmail.com>
2021-07-21 22:09:20 +0545

    adding comments

--- routes/articles.js
+++ routes/articles.js
@@ -3,6 +3,8 @@ const Article = require("./../models/article")
 
 const router = express.Router();
 
+//setting up  for new posts
+
 router.get("/new", (req, res) => {
   res.render ('articles/new', {article: new Article()});
 })
